872c12800f5417552049b0f769aa14e77f9faa4a
Amey Jakate <ameyjakate2001@gmail.com>
2021-02-05 06:48:59 -0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h2>{{ title }}</h2>
+      <AddTodo></AddTodo>
+      <!-- <MyTodos></MyTodos> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import addtodos from "./components/AddTodos.vue";
+import mytodos from "./components/mytodo.vue";
+
+export default {
+  components: {
+    AddTodo: addtodos,
+    MyTodos: mytodos,
+  },
+
+  name: "app",
+
+  data() {
+    return {
+      title: "Todo App",
+    };
+  },
+
+  methods: {},
+};
+</script>
+
+<style scoped>
+body{
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+
+.container {
+  max-width: 1200px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
